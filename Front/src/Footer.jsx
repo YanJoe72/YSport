13bb868bfee6d0787de81dfc523b503c76c1c1ec
YanJoe72 <yanisboidin@laposte.net>
2024-09-18 12:41:49 +0200

    version finale

--- Front/src/Footer.jsx
+++ Front/src/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
         <>
             <footer>
                 <div className="footer">
-                    <p>Créateurs :</p>
-                    <p>Lartigue Corentin, Majorczyk Vincent, Boidin Yanis, Barreda Gabriel, Sprumont Jimmy</p>
+                    <p>Créateur :</p>
+                    <p>Boidin Yanis</p>
                     <a href='zdazdz'>mentions légales</a>
                     <br></br>
                     <a href='fezfez'>politique de confidentialité</a>
